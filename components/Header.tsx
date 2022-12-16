45cec1520699bdc4c5f6905d25faa7e52cc3d5cd
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-15 22:48:38 -0800

    made heaeder responsive for tablet

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,13 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <div className="flex bg-[#373B53] w-full fixed top-0 h-[72px]">
-      <div className="w-[72px] h-full">
+      <div className="w-[72px] h-full md:w-20">
         <Image src={logo} alt="logo" />
       </div>
       <div className="h-full flex-grow pr-6 flex justify-end items-center">
         <Image src={lightDark} className="h-5" alt="light dark button" />
       </div>
-      <div className="h-full w-20 flex justify-center items-center border-l border-l-[#979797]/30">
+      <div className="h-full w-20 md:w-24 flex justify-center items-center border-l border-l-[#979797]/30">
         <Image
           src=""
           alt="profile image"
