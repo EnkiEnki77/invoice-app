2cc7f821d542c939ca842cb78eacb03962da2a66
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-15 23:02:45 -0800

    made header responsive for desktop

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,14 +7,17 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div className="flex bg-[#373B53] w-full fixed top-0 h-[72px]">
-      <div className="w-[72px] h-full md:w-20">
-        <Image src={logo} alt="logo" />
+    <header
+      className="flex bg-[#373B53] w-full fixed top-0 h-[72px] md:h-20 lg:flex-col lg:left-0 
+    lg:h-full lg:w-[103px] lg:rounded-r-[27px]"
+    >
+      <div className="w-[72px]  h-full md:w-20 lg:w-full lg:h-[103px]">
+        <Image src={logo} className="h-full w-full" alt="logo" />
       </div>
-      <div className="h-full flex-grow pr-6 flex justify-end items-center">
+      <div className="h-full lg:h-0 lg:flex-col lg:pb-8 lg:pr-0 flex-grow pr-6 flex justify-end items-center">
         <Image src={lightDark} className="h-5" alt="light dark button" />
       </div>
-      <div className="h-full w-20 md:w-24 flex justify-center items-center border-l border-l-[#979797]/30">
+      <div className="h-full w-20 md:w-24 lg:w-full lg:h-[88px] flex justify-center items-center border-l lg:border-t border-[#979797]/30">
         <Image
           src=""
           alt="profile image"
@@ -23,7 +26,7 @@ const Header = (props: Props) => {
           className="rounded-full border border-white"
         />
       </div>
-    </div>
+    </header>
   );
 };
 
